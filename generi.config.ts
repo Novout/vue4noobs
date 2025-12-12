cd8db524c7c2715b3bd1f2329f5c754faf0bf632
Novout <novout@hotmail.com>
2025-12-12 17:53:37 -0300

    feat: add generi

---
+++ generi.config.ts
@@ -0,0 +1,14 @@
+export default {
+	repository: "https://github.com/Novout/vue4noobs",
+	silent: false,
+	commits: "conventional-commits",
+	tag: true,
+	version: true,
+	push: true,
+	release: false,
+	publish: false,
+	exclude: [" typo"],
+	prerelease: "beta",
+	packagePath: "package.json",
+	lernaPath: "lerna.json"
+}
